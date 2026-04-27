ad7f3e5022347d70e5fe09aaed4091d78f1e6dff
A C E <A C E@HAMZA>
2026-04-27 04:40:39 +0300

    Add background job processing

---
+++ DentifySystem/BackgroundJobs/HangFireJobService.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire;
+using Service.Abstraction;
+
+namespace DentifySystem.BackgroundJobs
+{
+    public class HangfireJobService : IBackgroundJobService
+    {
+        public void ScheduleAppointmentCompletion(int appointmentId, DateTime runAt)
+        {
+            BackgroundJob.Schedule<IAppointmentService>(
+                service => service.AutoCompleteAppointmentAsync(appointmentId),
+                runAt);
+        }
+    }
+}
