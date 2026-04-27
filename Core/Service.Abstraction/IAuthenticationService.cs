1b0f288c2f9759d48be298d8f3021f4115ae14f4
A C E <A C E@HAMZA>
2026-04-27 04:55:34 +0300

    Ignore uploaded files and finalize project setup

---
+++ Core/Service.Abstraction/IAuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using Shared.CommonResult;
+using Shared.DTOs.IdentityDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Abstraction
+{
+    public interface IAuthenticationService
+    {
+        // Login
+        //Email ,Password => Token ,Email ,DisplayName
+        Task<Result<UserDTO>> LoginAsyns(LoginDTO loginDto);
+
+        //Register Patient
+        //Email, Password , UserName ,PhoneNumber ,DisplayName => Token ,Email ,DisplayName
+        Task<Result<UserDTO>> RegisterPatientAsync(RegisterPatientDTO dto);
+
+        //Register Doctor
+        // Email, Password , UserName ,PhoneNumber ,DisplayName,Bio,Fees,SpecializationId => Token ,Email ,DisplayName
+        Task<Result<UserDTO>> RegisterStudentAsync(RegisterStudentDTO dto);
+
+        Task<Result<CurrentUserDTO>> GetCurrentUserAsync(string userId);
+
+        Task<bool> CheckEmailAsync(string email);
+
+    }
+
+}
+
+
+
