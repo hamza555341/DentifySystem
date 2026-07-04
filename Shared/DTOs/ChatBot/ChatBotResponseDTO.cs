431004e3d2523bc9ddf03dc006ad173518b16e18
Mega Store <Mega Store@ZBOOK>
2026-07-05 01:59:47 +0300

    modify username

---
+++ Shared/DTOs/ChatBot/ChatBotResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.ChatBot
+{
+    public class ChatBotResponseDTO
+    {
+        public string Answer { get; set; } = null!;
+    }
+}
